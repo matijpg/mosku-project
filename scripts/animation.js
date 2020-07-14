7d97862c5f05754fe4e979bd4c8a13bf4cdabe10
mati__raw <hernanmatiassierra@gmail.com>
2020-07-13 23:23:48 -0300

    Añadimos animaciones

---
+++ scripts/animation.js
@@ -0,0 +1,50 @@
+//Navbar
+window.sr = ScrollReveal();
+sr.reveal(".navbar", {
+  delay: 375,
+  duration: 2000,
+  origin: "top",
+  easing: "ease-in",
+});
+
+sr.reveal(".text-frase, .title-header", {
+  duration: 1000,
+  origin: "top",
+  easing: "ease-in",
+  distance: "200px",
+});
+
+sr.reveal(".text-frase", {
+  duration: 1200,
+  origin: "top",
+  easing: "ease-in",
+  distance: "200px",
+});
+
+sr.reveal(".btn-vermas", {
+  duration: 1200,
+  origin: "bottom",
+  easing: "ease-in",
+  distance: "200px",
+});
+
+sr.reveal(".card-left", {
+  duration: 1200,
+  origin: "left",
+  easing: "ease-in",
+  distance: "200px",
+});
+
+sr.reveal(".card-rigth", {
+  duration: 1200,
+  origin: "rigth",
+  easing: "ease-in",
+  distance: "200px",
+});
+
+sr.reveal(".btn-turno ", {
+  duration: 1200,
+  origin: "bottom",
+  easing: "ease-in",
+  distance: "100px",
+});
